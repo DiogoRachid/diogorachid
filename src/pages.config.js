f23be3d5aed4231ef0a19394d1183812a670e6d6
Diogo Rachid <di.rachid@gmail.com>
2026-01-22 13:12:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -52,6 +52,7 @@ import TimeSheet from './pages/TimeSheet';
 import Transactions from './pages/Transactions';
 import VersionHistory from './pages/VersionHistory';
 import VersionHistoryForm from './pages/VersionHistoryForm';
+import BudgetPlanner from './pages/BudgetPlanner';
 import __Layout from './Layout.jsx';
 
 
@@ -110,6 +111,7 @@ export const PAGES = {
     "Transactions": Transactions,
     "VersionHistory": VersionHistory,
     "VersionHistoryForm": VersionHistoryForm,
+    "BudgetPlanner": BudgetPlanner,
 }
 
 export const pagesConfig = {
