cf0149976ff230c79040a40cc40c0d0aa1d49298
Diogo Rachid <di.rachid@gmail.com>
2026-01-22 20:00:17 +0000

    File changes

--- functions/debugProjectStages.ts
+++ functions/debugProjectStages.ts
@@ -48,7 +48,7 @@ Deno.serve(async (req) => {
 
     return Response.json({
       success: true,
-      budgetId,
+      finalBudgetId,
       totalStages: stages.length,
       stages: stagesDebug
     });
