f6d83e6b3a579acf46d39705c8bf5e3d439eed91
Diogo Rachid <di.rachid@gmail.com>
2026-03-24 11:54:34 +0000

    File changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -205,7 +205,7 @@ export default function LandingPage() {
       <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0">
           <img
-            src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1600&q=85"
+            src={heroImagem}
             alt="Obras"
             className="w-full h-full object-cover" />
           
